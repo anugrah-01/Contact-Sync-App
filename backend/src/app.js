09f7f2137e36a920647774ffe03036959f7618cd
anugrah-01 <anugrahsri001@gmail.com>
2026-03-14 01:50:26 +0530

    Implemented React frontend auth: login, register, API integration with backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";    
 import healthRoutes from "./routes/healthRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import contactRoutes from "./routes/contactRoutes.js";
@@ -6,6 +7,7 @@ import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", healthRoutes);
 app.use("/api/auth", authRoutes);
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import axios from "axios";
+
+function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault();
+
+        try {
+            const res = await axios.post("http://localhost:3000/api/auth/register",{
+                email,
+                password
+            });
+            console.log(res.data);
+
+        } catch (error) {
+            console.error(error.response?.data || error.message);
+        }
+    }
+    return(
+        <div>
+            <h1>Register Page</h1>
+
+            <form onSubmit={handleSubmit}>
+                <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required/>
+                <br />
+                <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required/>
+                <br />
+
+                <button type="submit">Register</button>
+            </form>
+
+        </div>
+    );
+}
+
+export default Register;
