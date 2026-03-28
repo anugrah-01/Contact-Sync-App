e98c853812a5a648533b5bbf38ac66d6483673d9
anugrah-01 <anugrahsri001@gmail.com>
2026-03-28 21:54:46 +0530

    Fix: Removed spaces

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -9,7 +9,7 @@ function Register() {
         e.preventDefault();
 
         try {
-            const res = await axios.post(`${import.meta.env.VITE_API_URL }/api/auth/register`, {
+            const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/register`, {
                 email,
                 password
             });
