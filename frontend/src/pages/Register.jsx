18fc740284903c01b2c345da9cce6b5233cc3b2b
anugrah-01 <anugrahsri001@gmail.com>
2026-03-28 23:31:04 +0530

    Register page navigation changes

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
     const handleSubmit = async(e) =>{
         e.preventDefault();
@@ -14,6 +16,8 @@ function Register() {
                 password
             });
             console.log(res.data);
+
+            navigate("/dashboard");
 
         } catch (error) {
             console.error(error.response?.data || error.message);
