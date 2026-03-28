008d83453eb1bef9d604c258a46bf5d359e0c891
anugrah-01 <anugrahsri001@gmail.com>
2026-03-28 23:25:39 +0530

    Additional changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
