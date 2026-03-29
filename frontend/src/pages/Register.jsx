896cb19961b85d9a8f242d6701e3f5e3697bed97
anugrah-01 <anugrahsri001@gmail.com>
2026-03-29 17:25:20 +0530

    Additional changes

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -30,6 +30,8 @@ function Register() {
             <h1>Register Page</h1>
 
             <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)} required/>
+                <br />
                 <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required/>
                 <br />
                 <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required/>
